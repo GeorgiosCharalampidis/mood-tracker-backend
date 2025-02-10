d97d89e003b33d654ceb4f42dba59b65f4cc03cd
Georgios Charalampidis <36412280+GeorgiosCharalampidis@users.noreply.github.com>
2025-02-10 18:41:59 +0200

    Added GlobalExceptionHandler to handle exceptions

---
+++ src/main/java/com/moodtracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.moodtracker.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>("Invalid request body: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
